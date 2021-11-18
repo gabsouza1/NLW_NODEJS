f2cce568d4a6b85787d10de6e76fa6158f1420a1
Gabriel Souza <bielzinhow89@gmail.com>
2021-11-18 20:49:24 -0300

    finalizada

--- src/routes.ts
+++ src/routes.ts
@@ -4,16 +4,15 @@ import { CreateMessageController } from "./controller/CreateMessageController";
 import { GetLastThreeMessagesController } from "./controller/GetLastThreeMessagesController";
 import { ProfileUserController } from "./controller/ProfileUserController";
 import { ensureAuthenticate } from "./middleware/ensureAuthenticate";
-import cors from 'cors'
 
 
 const router = Router();
 
 
-router.post('/authenticate', cors(), new AuthenticateUserController().handle)
-router.post('/messages', cors(), ensureAuthenticate, new CreateMessageController().handle)
+router.post('/authenticate', new AuthenticateUserController().handle)
+router.post('/messages', ensureAuthenticate, new CreateMessageController().handle)
 router.get('/messages/lastthree',new GetLastThreeMessagesController().handle)
-router.get('/profile', cors(), ensureAuthenticate ,new ProfileUserController().handle)
+router.get('/profile', ensureAuthenticate ,new ProfileUserController().handle)
 
 
 export { router }
